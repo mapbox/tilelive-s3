2636071db099129da34d1f6bec7fc2d075dbc73e
Jeff Miccolis <jeff@miccolis.net>
2014-03-31 18:15:41 -0400

    Attempting to fix build, again

--- test/build-static.sh
+++ test/build-static.sh
@@ -5,13 +5,12 @@ export CFLAGS="$CFLAGS -fPIC"
 
 build_dir=$(pwd)
 
-mkdir ./tmp
-wget 'http://prdownloads.sourceforge.net/libpng/libpng-1.6.10.tar.gz?download' -O ./tmp/libpng-1.6.10.tar.gz
-wget 'http://prdownloads.sourceforge.net/libpng/libpng-1.6.10.tar.gz.asc?download' -O ./tmp/libpng-1.6.10.tar.gz.asc
+mkdir ./tmp && cd ./tmp
+wget 'http://prdownloads.sourceforge.net/libpng/libpng-1.6.10.tar.gz?download' -O ./libpng-1.6.10.tar.gz
+wget 'http://prdownloads.sourceforge.net/libpng/libpng-1.6.10.tar.gz.asc?download' -O ./libpng-1.6.10.tar.gz.asc
 gpg --keyserver pgp.mit.edu --recv-keys A16C640F
-gpg --verify ./tmp/libpng-1.2.51.tar.gz.asc
+gpg --verify ./libpng-1.2.51.tar.gz.asc
 
-cd ./tmp
 tar xzf libpng-1.2.51.tar.gz
 cd ./libpng-1.2.51
 ./configure --enable-shared --disable-shared --disable-dependency-tracking
