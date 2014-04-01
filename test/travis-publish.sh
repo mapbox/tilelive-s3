7bbc087cb9602c87c09b520db096c27375bff11b
Jeff Miccolis <jeff@miccolis.net>
2014-04-01 15:38:22 -0400

    use local path to node-pre-gyp binary [publish binary]

--- test/travis-publish.sh
+++ test/travis-publish.sh
@@ -13,13 +13,13 @@ COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 
 if [ "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE" ]; then
     npm install aws-sdk
-    node-pre-gyp package testpackage
-    node-pre-gyp publish info
+    ./node_modules/.bin/node-pre-gyp package testpackage
+    ./node_modules/.bin/node-pre-gyp publish info
 
     rm -rf build
     rm -rf lib/binding
     npm install --fallback-to-build=false
     npm test
 
-    node-pre-gyp info
+    ./node_modules/.bin/node-pre-gyp info
 fi
