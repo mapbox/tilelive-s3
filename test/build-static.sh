2ddc30a60b11183116af46a5056d44aee9bb2176
Jeff Miccolis <jeff@miccolis.net>
2014-04-01 09:28:42 -0400

    corret path to gpg sig

--- test/build-static.sh
+++ test/build-static.sh
@@ -9,7 +9,7 @@ mkdir ./tmp && cd ./tmp
 wget 'http://prdownloads.sourceforge.net/libpng/libpng-1.6.10.tar.gz?download' -O ./libpng-1.6.10.tar.gz
 wget 'http://prdownloads.sourceforge.net/libpng/libpng-1.6.10.tar.gz.asc?download' -O ./libpng-1.6.10.tar.gz.asc
 gpg --keyserver pgp.mit.edu --recv-keys A16C640F
-gpg --verify ./libpng-1.2.51.tar.gz.asc
+gpg --verify ./libpng-1.6.10.tar.gz.asc
 
 tar xzf libpng-1.2.51.tar.gz
 cd ./libpng-1.2.51
