b53b2b09bd9496896dc45f812e4de1452df91cd3
Jeff Miccolis <jeff@miccolis.net>
2014-04-01 14:41:29 -0400

    Use proper version string [publish binary]

--- test/build-static.sh
+++ test/build-static.sh
@@ -6,9 +6,9 @@ export CFLAGS="$CFLAGS -fPIC"
 build_dir=$(pwd)
 
 mkdir ./tmp && cd ./tmp
-wget https://mapnik.s3.amazonaws.com/deps/libpng-1.6.9.tar.gz -O ./libpng-1.6.10.tar.gz
-tar xzf libpng-1.6.10.tar.gz
-cd ./libpng-1.6.10
+wget https://mapnik.s3.amazonaws.com/deps/libpng-1.6.9.tar.gz -O ./libpng-1.6.9.tar.gz
+tar xzf libpng-1.6.9.tar.gz
+cd ./libpng-1.6.9
 ./configure --enable-shared --disable-shared --disable-dependency-tracking
 make
 sudo make install
