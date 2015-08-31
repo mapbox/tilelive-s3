a895926aaa690fb68a7e6b7542a16e54a0ff3de0
Ryan Clark <ryan.clark.j@gmail.com>
2015-08-31 14:14:45 -0700

    retry timeouts, refactor error tests

--- test/error.test.js
+++ test/error.test.js
@@ -6,54 +6,111 @@ var fs = require('fs');
 var fixtures = path.resolve(__dirname + '/fixtures');
 var AWS = require('aws-sdk');
 
+function error(code, message) {
+    message = message || 'unknown error';
+    return '<?xml version="1.0" encoding="UTF-8"?>\n' +
+        '<Error>\n' +
+        '  <Code>' + code + '</Code>\n' +
+        '  <Message>' + message + '</Message>\n' +
+        '</Error>';
+}
+
+var attempts = 0;
+var png = fs.readFileSync(fixtures + '/tile.png');
+
 var mock = http.createServer(function (req, res) {
-    if (req.method == 'GET' && req.url.match(/^\/slowget(\/\d){3}.png$/)) {
-        setTimeout(function() {
+    attempts++;
+    var routes = {
+        slow: /^\/slow(get|put)(\/\d){3}.png$/,
+        missing: /^\/missingget(\/\d){3}.png$/,
+        hangup: /^\/hangup(get|put)(\/\d){3}.png$/,
+        rate: /^\/rate(get|put)(\/\d){3}.png$/,
+        internal: /^\/internal(get|put)(\/\d){3}.png$/,
+        nobody: /^\/nobodyput(\/\d){3}.png$/
+    };
+
+
+    if (routes.slow.test(req.url)) {
+        return setTimeout(function() {
             res.writeHead(200);
             res.end();
-        }, 10000);
-    } else if (req.method == 'GET') {
-        res.writeHead(404);
-        res.end();
-    } else if (req.method == 'PUT' && req.url.match(/^\/slowput(\/\d){3}.png$/)) {
-        setTimeout(function() {
-            res.writeHead(200);
-            res.end();
-        }, 10000);
-    } else if (req.method == 'PUT' && req.url.match(/^\/hangup(\/\d){3}.png$/)) {
-        req.socket.destroy();
-    } else if (req.method == 'PUT' && req.url.match(/^\/httperror(\/\d){3}.png$/)) {
-        var body = '<?xml version="1.0" encoding="UTF-8"?>\n' +
-                   '<Error>\n' +
-                   '  <Code>SlowDown</Code>\n' +
-                   '  <Message>Reduce your request rate</Message>\n' +
-                   '</Error>';
-        res.writeHead(503);
-        res.end(body);
-    } else if (req.method == 'PUT' && req.url.match(/^\/httperrornobody(\/\d){3}.png$/)) {
-        res.writeHead(503);
-        res.end();
-    } else {
-        res.writeHead(404);
+        }, req.method.toLowerCase() === req.url.match(routes.slow)[1] ? 5500 : 10);
+    }
+
+    if (routes.missing.test(req.url)) {
+        res.writeHead(req.method === 'GET' ? 404 : 200);
+        return res.end();
+    }
+
+    if (routes.hangup.test(req.url)) {
+        if (req.method.toLowerCase() === req.url.match(routes.hangup)[1])
+            return req.socket.destroy();
+
+        res.writeHead(200);
         res.end();
     }
+
+    if (routes.rate.test(req.url)) {
+        if (req.method.toLowerCase() === req.url.match(routes.rate)[1]) {
+            res.writeHead(503);
+            return res.end(error('SlowDown', 'Reduce your request rate'));
+        }
+
+        res.writeHead(200);
+        res.end();
+    }
+
+    if (routes.internal.test(req.url)) {
+        if (req.method.toLowerCase() === req.url.match(routes.internal)[1]) {
+            res.writeHead(500);
+            return res.end(error('InternalError'));
+        }
+
+        res.writeHead(200);
+        res.end();
+    }
+
+    if (routes.nobody.test(req.url)) {
+        console.log(req.method, req.url)
+        if (req.method === 'PUT') {
+            res.writeHead(503);
+            return res.end();
+        }
+
+        res.writeHead(200);
+        res.end();
+    }
+
+    res.writeHead(404);
+    res.end();
 });
 
-tape('start mock', function(assert) {
-    mock.listen(20009, function() {
-        // aws-sdk overrides to get tilelive-s3 to talk to the mock server
+function test(msg, assertions) {
+    tape(msg, function(assert) {
         S3.agent = http.globalAgent;
         AWS.config.update({
             endpoint: new AWS.Endpoint('http://localhost:20009'),
             s3BucketEndpoint: true
         });
 
-        assert.end();
-    });
-});
+        mock.listen(20009, function(err) {
+            if (err) throw err;
 
-tape('putTile retry on GET timeout', function(assert) {
-    var png = fs.readFileSync(fixtures + '/tile.png');
+            var done = assert.end.bind(assert);
+            assert.end = function(err) {
+                mock.close(function(error) {
+                    if (error) throw error;
+                    attempts = 0;
+                    done(err);
+                });
+            };
+
+            assertions(assert);
+        });
+    });
+}
+
+test('getTile retry timeout', function(assert) {
     new S3({
         data: {
             tiles: [ 'http://dummy-bucket.s3.amazonaws.com/slowget/{z}/{x}/{y}.png' ]
@@ -64,79 +121,276 @@ tape('putTile retry on GET timeout', function(assert) {
         source.startWriting(function(err) {
             if (err) return done(err);
             source.getTile(3, 6, 5, function(err) {
-                assert.equal(err.message, 'Timed out after 5000ms');
-                assert.equal(err.status, 504);
+                assert.equal(err.message, 'Timed out after 5000ms', 'expected message');
+                assert.equal(err.status, 504, 'expected status');
+                assert.equal(attempts, 5, 'retried 4 times');
                 assert.end();
             });
         });
     });
 });
 
-tape('putTile retry on PUT timeout', function(assert) {
-    var png = fs.readFileSync(fixtures + '/tile.png');
+test('putTile fail on GET timeout', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/slowget/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
 
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.putTile(3, 6, 5, png, function(err) {
+                assert.equal(err.message, 'Timed out after 5000ms', 'expected message');
+                assert.equal(err.status, 504, 'expected status');
+                assert.equal(attempts, 5, 'retried GET 4 times, no put attempt');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('putTile retry on PUT timeout', function(assert) {
     new S3({
         data: {
             tiles: [ 'http://dummy-bucket.s3.amazonaws.com/slowput/{z}/{x}/{y}.png' ]
         }
     }, function(err, source) {
         assert.ifError(err);
+
         source.startWriting(function(err) {
             if (err) return done(err);
             source.putTile(3, 6, 5, png, function(err) {
-                assert.equal(err.message, 'Timed out after 5000ms');
-                assert.equal(err.status, 504);
+                assert.equal(err.message, 'Timed out after 5000ms', 'expected message');
+                assert.equal(err.status, 504, 'expected status');
+                assert.equal(attempts, 6, '1 GET and retried PUT 4 times');
                 assert.end();
             });
         });
     });
 });
 
-tape('putTile retry on server closed connection', function(assert) {
-    var png = fs.readFileSync(fixtures + '/tile.png');
-
+test('getTile do not retry missing', function(assert) {
     new S3({
         data: {
-            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/hangup/{z}/{x}/{y}.png' ]
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/missingget/{z}/{x}/{y}.png' ]
         }
     }, function(err, source) {
         assert.ifError(err);
+
         source.startWriting(function(err) {
             if (err) return done(err);
-            source.putTile(3, 6, 5, png, function(err) {
-                assert.equal(err.message, 'socket hang up');
-                assert.equal(err.code, 'ECONNRESET');
+            source.getTile(3, 6, 5, function(err) {
+                assert.equal(err.status, 404, 'expected status');
+                assert.equal(attempts, 1, 'did not retry');
                 assert.end();
             });
         });
     });
 });
 
-tape('putTile retry on http error', function(assert) {
-    var png = fs.readFileSync(fixtures + '/tile.png');
-
+test('putTile do not retry GET missing', function(assert) {
     new S3({
         data: {
-            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/httperror/{z}/{x}/{y}.png' ]
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/missingget/{z}/{x}/{y}.png' ]
         }
     }, function(err, source) {
         assert.ifError(err);
+
         source.startWriting(function(err) {
             if (err) return done(err);
             source.putTile(3, 6, 5, png, function(err) {
-                assert.equal(err.message, 'Reduce your request rate');
+                assert.ifError(err, 'successful put');
+                assert.equal(attempts, 2, 'Did not retry GET');
                 assert.end();
             });
         });
     });
 });
 
-tape('putTile retry on http error (no body)', function(assert) {
-    var png = fs.readFileSync(fixtures + '/tile.png');
-
+test('getTile retry hangups', function(assert) {
     new S3({
         data: {
-            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/httperrornobody/{z}/{x}/{y}.png' ]
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/hangupget/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.getTile(3, 6, 5, function(err) {
+                assert.equal(err.message, 'socket hang up', 'expected message');
+                assert.equal(err.status, 500, 'expected status');
+                assert.equal(attempts, 5, 'retried 4 times');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('putTile fails on GET hangup', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/hangupget/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.putTile(3, 6, 5, png, function(err) {
+                assert.equal(err.message, 'socket hang up', 'expected message');
+                assert.equal(err.status, 500, 'expected status');
+                assert.equal(attempts, 5, 'retried GET 4 times');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('putTile retry on PUT hangup', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/hangupput/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.putTile(3, 6, 5, png, function(err) {
+                assert.equal(err.message, 'socket hang up', 'expected message');
+                assert.equal(err.status, 500, 'expected status');
+                assert.equal(attempts, 6, 'retried PUT 4 times');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('getTile do not retry unmanaged http error', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/rateget/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.getTile(3, 6, 5, function(err) {
+                assert.equal(err.message, 'Reduce your request rate', 'expected message');
+                assert.equal(attempts, 1, 'did not retry');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('putTile fails on GET unmanaged http error', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/rateget/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.putTile(3, 6, 5, png, function(err) {
+                assert.equal(err.message, 'Reduce your request rate', 'expected message');
+                assert.equal(err.status, 503, 'expected status');
+                assert.equal(attempts, 1, 'no retries, no put attempt');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('putTile fails on PUT unmanaged http error', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/rateput/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.putTile(3, 6, 5, png, function(err) {
+                assert.equal(err.message, 'Reduce your request rate', 'expected message');
+                assert.equal(err.status, 503, 'expected status');
+                assert.equal(attempts, 2, 'no retries');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('getTile retry internal error', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/internalget/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.getTile(3, 6, 5, function(err) {
+                assert.equal(err.message, 'unknown error', 'expected message');
+                assert.equal(err.status, 500, 'expected status');
+                assert.equal(attempts, 5, 'retried 4 times');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('putTile fails on GET internal error', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/internalget/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.putTile(3, 6, 5, png, function(err) {
+                assert.equal(err.message, 'unknown error', 'expected message');
+                assert.equal(err.status, 500, 'expected status');
+                assert.equal(attempts, 5, 'retried GET 4 times, no put attempt');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('putTile retry on PUT internal error', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/internalput/{z}/{x}/{y}.png' ]
+        }
+    }, function(err, source) {
+        assert.ifError(err);
+
+        source.startWriting(function(err) {
+            if (err) return done(err);
+            source.putTile(3, 6, 5, png, function(err) {
+                assert.equal(err.message, 'unknown error', 'expected message');
+                assert.equal(err.status, 500, 'expected status');
+                assert.equal(attempts, 6, 'retried PUT 4 times');
+                assert.end();
+            });
+        });
+    });
+});
+
+test('putTile no retry on PUT http error (no body)', function(assert) {
+    new S3({
+        data: {
+            tiles: [ 'http://dummy-bucket.s3.amazonaws.com/nobodyput/{z}/{x}/{y}.png' ]
         }
     }, function(err, source) {
         assert.ifError(err);
@@ -144,12 +398,10 @@ tape('putTile retry on http error (no body)', function(assert) {
             if (err) return done(err);
             source.putTile(3, 6, 5, png, function(err) {
                 assert.equal(err.message, 'S3 put failed: 503 Unknown');
+                assert.equal(err.status, 503, 'expected status');
+                assert.equal(attempts, 2, 'no retries');
                 assert.end();
             });
         });
     });
-});
-
-tape('mock server teardown', function(assert) {
-    mock.close(assert.end);
 });
