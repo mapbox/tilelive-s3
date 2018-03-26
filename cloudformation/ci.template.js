2a2bfb04b7fc128263a1785ffffe49cd78fd3079
Edgar Marca <matiskay@gmail.com>
2018-03-26 14:40:17 -0500

    Move the ci template to cloudformation folder

---
+++ cloudformation/ci.template.js
@@ -0,0 +1,88 @@
+'use strict';
+const cf = require('@mapbox/cloudfriend');
+
+
+module.exports = {
+  'AWSTemplateFormatVersion': '2010-09-09',
+  'Description': 'tilelive-s3 build resources',
+  'Resources': {
+    'BuildUser': {
+      'Type': 'AWS::IAM::User',
+      'Properties': {
+        'Policies': [
+          {
+            'PolicyName': 'buildList',
+            'PolicyDocument': {
+              'Statement': [
+                {
+                  'Action': [
+                    's3:ListBucket'
+                  ],
+                  'Effect': 'Allow',
+                  'Resource': [
+                    'arn:aws:s3:::mapbox-node-binary'
+                  ]
+                }
+              ]
+            }
+          },
+          {
+            'PolicyName': 'build',
+            'PolicyDocument': {
+              'Statement': [
+                {
+                  'Action': [
+                    's3:DeleteObject',
+                    's3:GetObject',
+                    's3:GetObjectAcl',
+                    's3:PutObject',
+                    's3:PutObjectAcl'
+                  ],
+                  'Effect': 'Allow',
+                  'Resource': [
+                    'arn:aws:s3:::mapbox-node-binary/tilelive-s3/*'
+                  ]
+                }
+              ]
+            }
+          },
+          {
+            'PolicyName': 'test',
+            'PolicyDocument': {
+              'Statement': [
+                {
+                  'Action': [
+                    's3:DeleteObject',
+                    's3:HeadObject',
+                    's3:GetObject',
+                    's3:GetObjectAcl',
+                    's3:PutObject',
+                    's3:PutObjectAcl'
+                  ],
+                  'Effect': 'Allow',
+                  'Resource': [
+                    'arn:aws:s3:::mapbox/tilelive-s3/*'
+                  ]
+                }
+              ]
+            }
+          }
+        ]
+      }
+    },
+    'BuildUserKey': {
+      'Type': 'AWS::IAM::AccessKey',
+      'Properties': {
+        'UserName': cf.ref('BuildUser')
+      }
+    }
+  },
+  'Outputs': {
+    'AccessKeyId': {
+      'Value': cf.ref('BuildUserKey')
+    },
+    'SecretAccessKey': {
+      'Value': cf.getAtt('BuildUserKey', 'SecretAccessKey')
+    }
+  }
+};
